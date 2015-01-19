c9d55fcc07f4e886922038ac26f3a73dc2e07ca4
Thomas Fiedler <thfied@gmail.com>
2015-01-19 10:01:51 +0100

    encapsulate into class and make countUp private

--- Ruby/excercise1/fibonacci.rb
+++ Ruby/excercise1/fibonacci.rb
@@ -1,21 +1,30 @@
-def fibo1(n)
-	if (n == 1 or n == 2) then 1
-	else fibo1(n-1) + fibo1(n-2)
+class Fibonacci 
+	def initialize(n)
+		@n = n
+	end
+	
+	def fibo1
+		if (@n == 1 or @n == 2) then 1
+		else fibo1(@n-1) + fibo1(@n-2)
+		end
+	end
+
+	def fibo2
+		if (@n == 1 or @n == 2) then 1
+		else countUp(@n, 2, 1, 1)
+		end
+	end
+	
+	private 
+	def countUp (n, x, acc, pred)
+		if x == n then acc
+		else countUp(n, x + 1, acc + pred, acc)
+		end
 	end
 end
 
 
-def countUp (n, x, acc, pred)
-	if x == n then acc
-	else countUp(n, x + 1, acc + pred, acc)
-	end
-end
+f = Fibonacci.new(3)
 
-def fibo2(n)
-	if (n == 1 or n == 2) then 1
-	else countUp(n, 2, 1, 1)
-	end
-end
-
-puts fibo1(3)
-puts fibo2(3)
+puts f.fibo1
+puts f.fibo2
